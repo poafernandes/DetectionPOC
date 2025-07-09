271047e12f06b983af580df325f829d09b899eeb
Alexandre Porto Alegre <s50.01@bradesco.com.br>
2025-07-09 14:39:51 -0300

    Initial Commit

---
+++ DetectionPOC/DetectionPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetectionPOCApp.swift
+//  DetectionPOC
+//
+//  Created by Alexandre Porto Alegre on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DetectionPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
